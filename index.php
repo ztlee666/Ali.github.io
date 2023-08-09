c2b17465754902513da35ee79be752f2e99c5374
Ade Ali Qosim <naviztlee@gmail.com>
2023-08-09 14:23:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>FORM NASI KOTAK</title>
+</head>
+
+<body>
+	<div class="container">
+		<div class="header">
+			<center><h1 class="judul">Form Pemesanan Nasi Kotak</h1></center>
+			<label class="mb-3">
+				<ul><label class="form-label">CABANG :</label>
+				<select class="form-control" name="">
+				<option value="masyarakat">-PILIH CABANG-</option>	
+				<option value="petugas">TALAGA</option>	
+				<option value="petugas">CIKIJING</option>	
+			</select></ul>
+
+		</ul>
+			<div class="mb-3">
+					<ul><label class="form-label">Nama Pelanggan : </label>
+					<input type="text" class="form-control" name="username" placeholder="Masukan Nama" required></ul>
+						<div class="mb-3">
+					<ul><label class="form-label">Nomor HP : </label>
+					<input type="text" class="form-control" name="username" placeholder="Masukan No HP" required></ul>
+					<div class="mb-3">
+					<ul><label class="form-label">Jumlah Kotak : </label>
+					<input type="text" class="form-control" name="username" placeholder="Masukan Jumlah Kotak" required></ul>
+					<div class="card-footer">
+			<ul><button type="submit" name="" class="btn btn-primary">PESAN</button>
+			
+			
+
+
+</body>
+</html>
